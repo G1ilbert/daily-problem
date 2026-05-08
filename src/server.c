384b2e4467add3757d8075881bed89c90787dfa5
G1ilbert <putti.oat@gmail.com>
2026-05-08 21:42:43 +0700

    day(001): basic HTTP server with hardcoded redirect

---
+++ src/server.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+#define PORT 8080
+#define BUFFER_SIZE 4096
+
+int main() {
+    int server_fd, client_fd;
+    struct sockaddr_in address;
+    char buffer[BUFFER_SIZE];
+    int addrlen = sizeof(address);
+
+    // 1. สร้าง socket
+    server_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (server_fd == 0) {
+        perror("socket failed");
+        exit(1);
+    }
+
+    // 2. ผูก socket กับ port
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(PORT);
+
+    if (bind(server_fd, (struct sockaddr*)&address, sizeof(address)) < 0) {
+        perror("bind failed");
+        exit(1);
+    }
+
+    // 3. รอรับ connection
+    listen(server_fd, 10);
+    printf("Server running on http://localhost:%d\n", PORT);
+
+    while(1) {
+        // 4. รับ connection จาก client
+        client_fd = accept(server_fd, (struct sockaddr*)&address,
+                          (socklen_t*)&addrlen);
+
+        // 5. อ่าน request
+        memset(buffer, 0, BUFFER_SIZE);
+        read(client_fd, buffer, BUFFER_SIZE);
+        printf("Request:\n%s\n", buffer);
+
+        // 6. ส่ง redirect response
+        char* response =
+            "HTTP/1.1 302 Found\r\n"
+            "Location: https://google.com\r\n"
+            "Content-Length: 0\r\n"
+            "\r\n";
+
+        write(client_fd, response, strlen(response));
+        close(client_fd);
+    }
+
+    close(server_fd);
+    return 0;
+}
